816a682a2633e18636d64d9e99d95eb695cefd17
Tibaud <et@sumatra.fr>
2018-02-14 01:40:09 +0100

    2018

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Gossipez vos amis à coups de Pin >> https://desolate-caverns-79675.herokuapp.com/
 
 . on ne peut éditer que ses gossips en étant connecté
-. on se connecte avec un username, on sign-up avec username et mail
+. on sign-in avec un username, on sign-up avec username et mail
+. on doit rentrer le code 2018 pour sign up
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :pins
+  validates :username, presence: true
+  attr_accessor :sign_up_code
+  validates :sign_up_code,
+    on: :create,
+    presence: true,
+    inclusion: { in: ["2018"] }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :pins
 end
